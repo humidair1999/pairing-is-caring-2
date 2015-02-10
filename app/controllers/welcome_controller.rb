ad682e3842c59b659f4d073d65a50b1fd0a5f3a1
J. Ky Marsh <jkymarsh@gmail.com>
2015-02-09 18:48:51 -0800

    remove all appointment request files

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,5 @@
 class WelcomeController < ApplicationController
     def index
-        @appointments = Appointment.all
+        
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,19 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150207080411) do
+ActiveRecord::Schema.define(version: 20150207072151) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "appointment_requests", force: true do |t|
-    t.datetime "desired_for"
-    t.string   "city"
-    t.text     "notes"
-    t.integer  "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
 
   create_table "appointments", force: true do |t|
     t.datetime "scheduled_for"
