3a617d8f8ec238c709eb5b38377ffd16b35c7583
J. Ky Marsh <jkymarsh@gmail.com>
2015-02-11 17:57:12 -0800

    begin writing user specs

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,8 +22,8 @@ class User < ActiveRecord::Base
 
     # returns the hash digest of the given string
     def User.digest(string)
-        cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                                      BCrypt::Engine.cost
+        cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+
         BCrypt::Password.create(string, cost: cost)
     end
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,21 +1,45 @@
 require "rails_helper"
 
 RSpec.describe User, :type => :model do
-    it { should have_secure_password }
+    describe "validations" do
+        it { should have_secure_password }
 
-    it { should validate_presence_of(:username) }
-    it { should validate_presence_of(:email) }
-    it { should validate_presence_of(:password) }
+        it { should validate_presence_of(:username) }
+        it { should validate_presence_of(:email) }
+        it { should validate_presence_of(:password) }
 
-    # apparently validate_length_of is only supported on master branch release
-    #  of shoulda_matchers
-    it { should ensure_length_of(:email).is_at_most(160) }
-    it { should ensure_length_of(:username).is_at_most(30) }
+        # apparently validate_length_of is only supported on master branch release
+        #  of shoulda_matchers
+        it { should ensure_length_of(:email).is_at_most(160) }
+        it { should ensure_length_of(:username).is_at_most(30) }
 
-    it { should have_many(:appointments) }
+        it { should have_many(:appointments) }
+    end
 
-    it "has a valid factory" do
-        expect(FactoryGirl.create(:user)).to be_valid
+    describe "instantiation" do
+        let(:user) { FactoryGirl.create(:user) }
+
+        it "has a valid factory" do
+            expect(user).to be_valid
+        end
+    end
+
+    describe "User.digest" do
+        it "should produce a bcrypt password" do
+            fake_digest = User.digest('fake string')
+
+            expect(fake_digest).to be_a(String)
+            expect(fake_digest.length).to be > 10
+        end
+    end
+
+    describe "User.new_token" do
+        it "should produce a base64 string" do
+            fake_token = User.new_token
+
+            expect(fake_token).to be_a(String)
+            expect(fake_token.length).to be > 10
+        end
     end
 
     # it "orders by last name" do
