ccb6a9aab91ec50aecf42ff6f52c3961ccf6e342
J. Ky Marsh <jkymarsh@gmail.com>
2015-02-12 01:07:05 -0800

    finish spec for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,51 +1,90 @@
 require "rails_helper"
 
 RSpec.describe User, :type => :model do
-    describe "validations" do
-        it { should have_secure_password }
+    describe "class functionality" do
+        describe "validations" do
+            it { should have_secure_password }
 
-        it { should validate_presence_of(:username) }
-        it { should validate_presence_of(:email) }
-        it { should validate_presence_of(:password) }
+            it { should validate_presence_of(:username) }
+            it { should validate_presence_of(:email) }
+            it { should validate_presence_of(:password) }
 
-        # apparently validate_length_of is only supported on master branch release
-        #  of shoulda_matchers
-        it { should ensure_length_of(:email).is_at_most(160) }
-        it { should ensure_length_of(:username).is_at_most(30) }
+            # apparently validate_length_of is only supported on master branch release
+            #  of shoulda_matchers
+            it { should ensure_length_of(:email).is_at_most(160) }
+            it { should ensure_length_of(:username).is_at_most(30) }
 
-        it { should have_many(:appointments) }
+            it { should have_many(:appointments) }
+        end
+
+        describe "User.digest" do
+            it "should produce a bcrypt password" do
+                fake_digest = User.digest('fake string')
+
+                expect(fake_digest).to be_a(String)
+                expect(fake_digest.length).to be > 10
+            end
+        end
+
+        describe "User.new_token" do
+            it "should produce a base64 string" do
+                fake_token = User.new_token
+
+                expect(fake_token).to be_a(String)
+                expect(fake_token.length).to be > 10
+            end
+        end
     end
 
-    describe "instantiation" do
+    describe "instance functionality" do
         let(:user) { FactoryGirl.create(:user) }
 
         it "has a valid factory" do
             expect(user).to be_valid
         end
-    end
 
-    describe "User.digest" do
-        it "should produce a bcrypt password" do
-            fake_digest = User.digest('fake string')
+        describe "remember" do
+            it "assigns remember_token attribute to user" do
+                user.remember
 
-            expect(fake_digest).to be_a(String)
-            expect(fake_digest.length).to be > 10
+                expect(user.remember_token).not_to be nil
+            end
+
+            it "updates remember_digest attribute on user" do
+                user.remember
+
+                expect(user.remember_digest).not_to be nil
+            end
+        end
+
+        describe "authenticated?" do
+            it "should return false if the user doesn't have a remember_digest" do
+                expect(user.authenticated?('intentionally false token')).to be_falsy
+            end
+
+            it "should return false if the passed remember_token doesn't match user's stored one" do
+                user.remember
+
+                expect(user.authenticated?('intentionally false token')).to be_falsy
+            end
+
+            it "should return true if the passed remember_token matches user's stored one" do
+                user.remember
+
+                expect(user.authenticated?(user.remember_token)).to be_truthy
+            end
+        end
+
+        describe "forget" do
+            before(:each) do
+                user.remember
+            end
+
+            it "clears remember_digest attribute on user" do
+                user.forget
+
+                expect(user.remember_digest).to be nil
+            end
         end
     end
-
-    describe "User.new_token" do
-        it "should produce a base64 string" do
-            fake_token = User.new_token
-
-            expect(fake_token).to be_a(String)
-            expect(fake_token.length).to be > 10
-        end
-    end
-
-    # it "orders by last name" do
-    #     lindeman = User.create!(first_name: "Andy", last_name: "Lindeman")
-    #     chelimsky = User.create!(first_name: "David", last_name: "Chelimsky")
-
-    #     expect(User.ordered_by_last_name).to eq([chelimsky, lindeman])
-    # end
 end
